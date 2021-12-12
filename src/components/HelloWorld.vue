b62f36df8a1e85f8da1adc7d10715e5b638b5834
leoiris123 <oulei684@gmail.com>
2021-12-13 00:21:13 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div :id="chartId" :style="{ width: width, height: height }"></div>
   </div>
 </template>
 
 <script>
+import carbase from "../data/car.json"
 export default {
-  name: 'HelloWorld',
+  name: "MyChart",
   props: {
-    msg: String
+    chartId: {
+      type: String,
+      default: "asdasdas"
+    },
+    width: {
+      type: String,
+      default: "500px"
+    },
+    height: {
+      type: String,
+      default: "500px"
+    },
+    chartOptions: {
+      type: Object
+
+    },
+    
+  },
+  data(){
+    return {
+      car:"",
+      option : {
+        title: {
+          text: "小车走势"
+        },
+        tooltip: {},
+        legend: {
+          data: ["y轴"]
+        },
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            name: "价格",
+            data: [220, 202, 231, 243, 225, 220, 213],
+            type: "line",
+            smooth: true
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    this.initBaseData(carbase)
+    this.createChart()
+  },
+  methods: {
+    initBaseData(base){
+      console.log(base,"basedata")
+
+      // JSON.parse((base+"")）
+
+    },
+    createChart() {
+      // 基于准备好的dom，初始化echarts实例
+      let chart = this.$echarts.init(document.getElementById(this.chartId));
+      // 指定图表的配置项和数据
+      var option = this.option
+      option.xAxis.data=carbase.x  
+      option.series[0].data=carbase.y
+      chart.setOption(option);
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
